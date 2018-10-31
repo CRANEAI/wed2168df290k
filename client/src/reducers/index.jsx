f4f81d66af3765fd485be51ceb3750be5234b0bc
Elly <elly@mg.crane.ai>
2018-10-31 12:18:41 -0400

    updated client/src/reducers/index refactoring

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -2,8 +2,10 @@ import { combineReducers } from "redux"
 import { reducer as Form } from "redux-form"
 
 import signup from "./reducer_signup"
+import signup from "./reducer_signup"
 
 const rootReducer = combineReducers({
+  signup: signup,
   signup: signup
 })
 
